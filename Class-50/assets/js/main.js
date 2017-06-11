4caf2a6abdbc0b4d2adea65c1c6004aba011b60b
Nazmul Hasan <najmulrizvy@gmail.com>
2017-06-11 08:55:37 +0600

    Class 50

---
+++ Class-50/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".carowsel-listing").owlCarousel({
+            items: 3,
+            loop: true,
+            nav:false,
+            dots:true,
+            margin: 20,
+            autoplay: true,
+        });
+        
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	

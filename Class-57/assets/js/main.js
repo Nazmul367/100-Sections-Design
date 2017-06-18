1f6683958750e1829f4208b519c16d8e9267ec53
Nazmul Hasan <najmulrizvy@gmail.com>
2017-06-18 23:11:15 +0600

    Class 57

---
+++ Class-57/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function() {
+    
+    $(".testimonial-author-title").perfectScrollbar();
+        
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+

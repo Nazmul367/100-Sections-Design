7b37bebed0b1bce3ace1aec852184d662d7f09aa
Nazmul Hasan <najmulrizvy@gmail.com>
2017-10-28 00:17:28 +0600

    Class 93

---
+++ Class-93/assets/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+    
+     $(".slider-area").owlCarousel({
+         items:1,
+         loop:true,
+         dots:true,
+         autoplay:true,
+         nav:false
+     });
+    
+});
